627eb2b6946e1ff57f55356b64d39998135d8de7
EC2 Default User <ec2-user@ip-172-31-32-41.ap-northeast-1.compute.internal>
2025-02-22 16:41:06 +0000

    item2

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  blongs_to :user
+
+  private
+
+  def items_params
+    params.require(:item).permit(:name, :introduction, :price)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'items/index'
-  get 'items/show'
   devise_for :users
   root to: "homes#top"
+  resources :items, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
